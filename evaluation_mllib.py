fd9fe3c6f548c1c3f3583230c88ab5b62e4bdc69
Hengyu Tang <ht1162@login-1-1.local>
2019-05-06 20:26:04 -0400

    evaluation_mllib draft

---
+++ evaluation_mllib.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.mllib.recommendation import MatrixFactorizationModel
+from pyspark.mllib.evaluation import RankingMetrics
+
+def main(spark, model_file, test_file):
+    model = MatrixFactorizationModel.load(sc, model_file)
+    
+    test_df = spark.read.parquet(test_file)
+    test_df = test_df.select('user_label', 'track_label', 'count')
+    
+    #test_rdd = test_df.rdd.map(lambda x: (int(x[0]), int(x[1]), int(x[2])))
+ 
+
+    #predictions = model.recommendProductsForUsers(500)
+    predictions = model.recommendProductsForUsers(2)
+    prediction_flat = predictions.flatMap(lambda p: p[1])
+    prediction_df = prediction_flat.toDF()
+    intersections = prediction_df.join(test_df, (prediction_df.product == test_df.track_label)&
+                                      (prediction_df.user == test_df.user_label), how = 'inner')
+    predLabel = intersections.select('rating', 'count')
+    predLabel_rdd = predLabel.map(lambda x: (x[0], x[1]))
+    metrics = RankingMetrics(predLabl_rdd) 
+    print(metrics.precisionAt(2))
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName('modeling').getOrCreate()
+    sc = spark.sparkContext
+    
+    model_file = sys.argv[1]
+    test_file = sys.argv[2]
+
+    main(spark, model_file, test_file)
+
+
+
+
