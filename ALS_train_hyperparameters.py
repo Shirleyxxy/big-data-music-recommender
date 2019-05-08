fbd96eaad31e959537b58900f1a21fa6f61129d5
Nhung  Le <nhl256@nyu.edu>
2019-05-08 18:40:14 -0400

    Update ALS train and hyperparamter tunning

--- ALS_train_hyperparameters.py
+++ ALS_train_hyperparameters.py
@@ -6,6 +6,8 @@
 # PYSPARK_PYTHON=$(which python) pyspark
 
 # To run: 
+#full train: spark-submit ALS_train.py cf_train_transformed_v8.parquet cf_val_transformed_v1.parquet hdfs:/user/nhl256/als_train.model
+
 # spark-submit ALS_train.py hdfs:/user/nhl256/cf_val_transformed.parquet hdfs:/user/nhl256/cf_test_transformed.parquet hdfs:/user/nhl256/train_als_val_small.model
 
 
@@ -21,7 +23,6 @@ from pyspark.ml.recommendation import ALS
 # from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 
 
-
 def main(spark, train_file, val_file, model_file):
     
     train = spark.read.parquet(train_file)
@@ -35,66 +36,78 @@ def main(spark, train_file, val_file, model_file):
     als = ALS(maxIter = 5, regParam = 0.01, implicitPrefs = True, userCol = 'user_label', itemCol = 'track_label', ratingCol = 'count')
     
     als_model = als.fit(train_df)
-    val_preds = als_model.transform(val_df)
-    evaluator = RegressionEvaluator(metricName='rmse', labelCol='count', predictionCol='prediction')
-    rmse = evaluator.evaluate(val_preds)
-    print('Baseline Root-mean-square error = ', rmse)
-    # Baseline Root-mean-square error =  7.652006276502559
     
     
-    # Generate top 10 movie recommendations for each user
-    userRecs = als_model.recommendForAllUsers(10)
+    # Generate top 10 song recommendations for each user 
+    predictions = als_model.recommendForAllUsers(10)
+    prediction_df = predictions.rdd.map(lambda r: (r.user_label, [i[0] for i in r.recommendations])).toDF()
+    prediction_df = prediction_df.selectExpr("_1 as user_label", "_2 as recommendations")
     
-    # Hyper-parameters tunning 
-    ranks = [10, 20, 40, 80]
-    reg_params = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
-    alphas = [0.25, 0.50, 0.75, 1.00]
+    # Get the songs that each user listen to 
+    val_grouped = test_df.groupBy('user_label').agg(F.collect_list(F.col('track_label')).alias('track_label'))
+    
+    # Join table
+    val_pred = val_grouped.join(prediction_df, "user_label", "inner")
+    
+    # Instantiate regression metrics to compare predicted and actual ratings
+    rdd = val_pred.select('recommendations', 'track_label').rdd
+    ranking_metrics = RankingMetrics(rdd)
+
+    # MAP
+    print("MAP = %s" % ranking_metrics.meanAveragePrecision)
+    
+    
+
+    
+    ################## Hyper-parameters tunning ##################
+    ranks = [10, 20]
+    reg_params = [0.001, 0.005]
+    alphas = [0.25]
     best_rank = None
     best_reg_param = None
     best_alpha = None
     best_model = None
-    best_rmse = 1e9
+    best_map = 1e9
     
     for rank, alpha, reg_param in itertools.product(ranks, alphas, reg_params):
         als = ALS(maxIter = 5, regParam = reg_param, implicitPrefs = True, alpha = alpha, rank =rank, userCol = 'user_label', itemCol = 'track_label', ratingCol = 'count')
 
         model = als.fit(train_df)
-        val_preds  = model.transform(val_df)
-        rmse = evaluator.evaluate(val_preds)
+        # Look into top 100 songs recommended
+        predictions = model.recommendForAllUsers(100)
+        prediction_df = predictions.rdd.map(lambda r: (r.user_label, [i[0] for i in r.recommendations])).toDF()
+        prediction_df = prediction_df.selectExpr("_1 as user_label", "_2 as recommendations")
+        
+        
+        # Join table
+        true_pred = val_grouped.join(prediction_df, "user_label", "inner")
+    
+        # Instantiate regression metrics to compare predicted and actual ratings
+        rdd = val_pred.select('recommendations', 'track_label').rdd
+        ranking_metrics = RankingMetrics(rdd)
+
+        # MAP
+        map = ranking_metrics.meanAveragePrecision
+    
         print('Current rank:', rank)
         print('Current alpha:', alpha)
         print('Current reg:', reg_param) 
-        if rmse < best_rmse:
+        if map < best_map:
             best_rank = rank
             best_reg_param = reg_param
             best_alpha = alpha
             best_model = model
-            best_rmse = rmse 
+            best_map = map 
 
     print('Best rank:', best_rank)
     print('Best regParam:', best_reg_param)
     print('Best alpha:', best_alpha)
-    print('Best rmse:', best_rmse)
+    print('Best map:', best_map)
     
     # save the best model
     best_model.save(model_file)
 
     
-#     ####### Archived work using MLLiB ###########
-    
-#     # Try with sample of 10% of data first
-#     df = df.sample(False, 0.1)
-    
-#     # A Ratings object is made up of (user, item, rating)
-#     ratings = df.rdd.map(lambda x: Rating(int(x[4]), int(x[5]), float(x[1])))
-    
-#     #Setting up the parameters for ALS
-#     rank = 5 # Latent Factors to be made
-#     numIterations = 10 # Times to repeat process
-#     #Create the model on the training data
-#     model = ALS.trainImplicit(ratings, rank, numIterations)
-    
-#     model.save(spark.SparkContext, model_file)
 
 if __name__ == '__main__':
     
