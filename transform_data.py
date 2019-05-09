f5f0e99a0a65df2d8a8afb7d0d8823b13ed336fd
Hengyu Tang <tanghengyu@gmail.com>
2019-05-08 22:17:03 -0400

    sample_transform_draft

--- transform_data.py
+++ transform_data.py
@@ -10,6 +10,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline
+from pyspark.sql.functions import monotonically_increasing_id
 
 # train_file = 'hdfs:/user/bm106/pub/project/cf_train.parquet'
 # val_file = 'hdfs:/user/bm106/pub/project/cf_validation.parquet'
@@ -20,36 +21,49 @@ from pyspark.ml import Pipeline
 
 ## Command to run: spark-submit --driver-memory 16g --executor-memory 16g transform_data.py hdfs:/user/bm106/pub/project/cf_train.parquet hdfs:/user/bm106/pub/project/cf_validation.parquet hdfs:/user/bm106/pub/project/cf_test.parquet hdfs:/user/nhl256/cf_train_transformed_v8.parquet hdfs:/user/nhl256/cf_val_transformed_v1.parquet hdfs:/user/nhl256/cf_test_transformed_v1.parquet
 
-def main(spark, train_file, val_file, test_file, train_output_file, 
+def main(spark, train_file, val_file, test_file, meta_file, train_output_file,
          val_output_file, test_output_file):
-    
+
     train_data = spark.read.parquet(train_file)
     val_data = spark.read.parquet(val_file)
     test_data = spark.read.parquet(test_file)
-    
-    # Drop '__index_level_0__' columns 
+    meta_data = spark.read.parquet(meta_file)
+
+    # Drop '__index_level_0__' columns
     train_data = train_data.drop('__index_level_0__')
     val_data = val_data.drop('__index_level_0__')
     test_data = test_data.drop('__index_level_0__')
-    
-    
+
+    train_idx = train_data.select("*").withColumn("id", monotonically_increasing_id())
+    train_idx.creatOrReplaceTempView('train_idx')
+    train_subset = spark.sql('SELECT * FROM train_idx ORDER BY train_idx.id DESC LIMIT 6000000')
+
     user_indexer = StringIndexer(inputCol = 'user_id', outputCol = 'user_label', handleInvalid = 'skip')
     track_indexer = StringIndexer(inputCol = 'track_id', outputCol = 'track_label', handleInvalid = 'skip')
-    
-    indexer_pipeline = Pipeline(stages = [user_indexer, track_indexer])
-    
-    train_model = indexer_pipeline.fit(train_data)
-    train_data = train_model.transform(train_data)
-    val_data = train_model.transform(val_data)
-    test_data = train_model.transform(test_data)
-    
-    # Make sure that train, val, and test have been transformed 
+
+    #indexer_pipeline = Pipeline(stages = [user_indexer, track_indexer])
+
+    # train subset will contain all users from val and test
+    user_model = user_indexer.fit(train_subset)
+    # meta data track file will contain all track infos
+    track_model = track_indexer.fit(meta_data)
+
+    train_data = user_model.transform(train_subset)
+    train_data = track_model.transform(train_data)
+
+    val_data = user_model.transform(val_data)
+    val_data = track_model.transform(val_data)
+
+    test_data = user_model.transform(test_data)
+    test_data = track_model.transform(test_data)
+
+    # Make sure that train, val, and test have been transformed
     print(train_data.take(1))
     print(val_data.take(1))
     print(test_data.take(1))
-    
+
     # repartition the data frame prior to writing (fix for java.lang.OutOfMemoryError)
-    
+
     train_data = train_data.repartition(5000, 'user_label')
     val_data = val_data.repartition('user_label')
     test_data = test_data.repartition('user_label')
@@ -61,7 +75,7 @@ def main(spark, train_file, val_file, test_file, train_output_file,
 
 
 if __name__ == '__main__':
-    
+
     conf = SparkConf()
     conf.set('spark.executor.memory', '16g')
     conf.set('spark.driver.memory', '16g')
@@ -74,13 +88,14 @@ if __name__ == '__main__':
     train_file = sys.argv[1]
     val_file = sys.argv[2]
     test_file = sys.argv[3]
+    meta_file = sys.argv[4]
 
     # paths to store the transformed files
-    train_output_file = sys.argv[4]
-    val_output_file = sys.argv[5]
-    test_output_file = sys.argv[6]
-    
+    train_output_file = sys.argv[5]
+    val_output_file = sys.argv[6]
+    test_output_file = sys.argv[7]
+
 
     #Call the main function
-    main(spark, train_file, val_file, test_file, train_output_file, 
+    main(spark, train_file, val_file, test_file, meta_file, train_output_file,
          val_output_file, test_output_file)
