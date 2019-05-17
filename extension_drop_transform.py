727ffd2419badbc1728405481c683ed3c14aa341
Hengyu Tang <ht1162@login-1-1.local>
2019-05-17 13:20:04 -0400

    extension_drop_modification

--- extension_drop_transform.py
+++ extension_drop_transform.py
@@ -21,20 +21,7 @@ def main(spark, train_file, meta_file, train_new_file):
     train_data.createOrReplaceTempView('train_data')
     
     train_data = spark.sql('SELECT * FROM train_data WHERE train_data.count > 2')
-    train_idx = train_data.select("*").withColumn("id", monotonically_increasing_id())
-    train_idx.createOrReplaceTempView('train_idx')
-    train_subset = spark.sql('SELECT * FROM train_idx ORDER BY train_idx.id DESC LIMIT 6000000')
     print('Finish dropping the low counts')
-   
-    user_indexer = StringIndexer(inputCol = 'user_id', outputCol = 'user_label', handleInvalid = 'skip')
-    track_indexer = StringIndexer(inputCol = 'track_id', outputCol = 'track_label', handleInvalid ='skip')
-    
-    user_model = user_indexer.fit(train_subset)
-    track_model = track_indexer.fit(meta_data)
-     
-    train_data = user_model.transform(train_subset)
-    train_data = track_model.transform(train_data)
-    print('Finish trasnforming train data') 
    
     print('The rows remained in train:', train_data.first())
     train_data = train_data.repartition(5000, 'user_label')
