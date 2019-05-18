1db08c3269d507ce077ec22f8b09abb580a18dea
Hengyu Tang <tanghengyu@gmail.com>
2019-05-18 12:20:35 -0400

    minor_changes

--- Extensions/extension_log_transform.py
+++ Extensions/extension_log_transform.py
@@ -8,31 +8,21 @@ import pyspark.sql.functions as F
 
 # This script transform the dataset 
 # spark-submit --driver-memory 16g --executor-memory 16g extension_perform.py 
-# hdfs:/user/ht1162/train_new.parquet hdfs:/user/ht1162/val_new.parquet 
-# hdfs:/user/ht1162/test_new.parquet hdfs:/user/ht1162/train_log.parquet
-# hdfs:/user/ht1162/val_log.parquet hdfs:/user/ht1162/test_log.parquet 
+# hdfs:/user/ht1162/train_new.parquet hdfs:/user/ht1162/train_log.parquet
 
-def main(spark, train_file, val_file, test_file, train_log_file, val_log_file, test_log_file):
+def main(spark, train_file, train_log_file):
     train_data = spark.read.parquet(train_file)
-    val_data = spark.read.parquet(val_file)
-    test_data = spark.read.parque(test_file)
     print('Finishe reading all data')
-
-    train_log = train_data.withColumn('log_count', F.log('count'))
-    val_log = val_data.withColumn('log_count', F.log('count'))
-    test_log = test_data.withColumn('log_count', F.log('count'))
+    
+    #Log transform the counts for training set
+    train_data = train_data.withColumn('inc_count', train_data['count']+1)
+    train_log = train_data.withColumn('log_count', F.log(train_data['inc_count']))
     print('Finish adding a column')
 
     train_log = train_log.repartition(5000, 'user_label')
-    val_log = val_log.repartition('user_label')
-    test_log = test_log.repartition('user_label')    
 
     train_log.write.parquet(train_log_file)
     print('Finish writing training_data')
-    val_log.write.parquet(val_log_file)
-    print('Finish writing val_data')
-    test_log.write.parquet(test_log_file)
-    print('Finish writing test_data')
 
 
 if __name__ == '__main__':
@@ -47,14 +37,9 @@ if __name__ == '__main__':
 
     # paths for the original files
     train_file = sys.argv[1]
-    val_file = sys.argv[2]
-    test_file = sys.argv[3]
 
     # paths to store the transformed files
-    train_log_file = sys.argv[4]
-    val_log_file = sys.argv[5]
-    test_log_file = sys.argv[6]
+    train_log_file = sys.argv[2]
 
     #Call the main function
-    main(spark, train_file, val_file, test_file, train_log_file,
-         val_log_file, test_log_file)
+    main(spark, train_file, train_log_file)
